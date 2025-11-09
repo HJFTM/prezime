31232f693ec66c17db83fc79541c11dd9c86327b
HJFTM <homejularic@gmail.com>
2025-11-09 10:42:44 +0100

    Create menu_json.js

---
+++ scripts/menu_json.js
@@ -0,0 +1,20 @@
+/**
+ * Pretvori ugniježđeni menu.json u ravnu listu pathova
+ * @param {Object} menuJson - objekt iz menu.json (već parsiran iz datoteke)
+ * @returns {string[]} lista pathova (npr. ["/pages/ROD/prezime/Bosna", "/pages/ROD/generacije/Bosna", ...])
+ */
+function extractPathsFromMenu(menuJson) {
+  const paths = [];
+
+  function traverse(pages) {
+    if (!Array.isArray(pages)) return;
+    for (const item of pages) {
+      if (item.path) paths.push(item.path);
+      if (item.pages) traverse(item.pages);
+    }
+  }
+
+  // podrška za strukturu: { project: "...", pages: [...] }
+  if (menuJson.pages) traverse(menuJson.pages);
+  return paths;
+}
